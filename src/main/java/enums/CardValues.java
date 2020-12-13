074879b931ce9ec06298648e94ec34ed4b1b9cbd
Ignas Guzelis <igngzl@telesoftas.com>
2020-12-14 01:34:06 +0200

    Introduce Card class, Enums

---
+++ src/main/java/enums/CardValues.java
@@ -0,0 +1,17 @@
+package enums;
+
+public enum CardValues {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE,
+}
